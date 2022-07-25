f98cee8b2d5e597b7eacacc53a95e5c5d51cc118
chanzerolee <86837078+chanzerolee@users.noreply.github.com>
2022-07-25 13:56:55 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String [] args){
+        Scanner stdin=new Scanner(System.in);
+        String s;
+        s=stdin.next();
+        s+="??!";
+        System.out.println(s);
+    }
+}
