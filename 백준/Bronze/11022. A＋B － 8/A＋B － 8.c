a2c7d75127c47144cdeb6901c233871d8c73a049
chanzerolee <86837078+chanzerolee@users.noreply.github.com>
2022-07-25 14:11:05 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d",&c);
+    for(int i=1;i<=c;i++){
+        scanf("%d%d",&a,&b);
+        printf("Case #%d: %d + %d = %d\n",i,a,b,a+b);
+    }
+}
