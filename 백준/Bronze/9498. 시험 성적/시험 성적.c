37cb947561617eb7ccf7723e4bec794943e6e157
chanzerolee <86837078+chanzerolee@users.noreply.github.com>
2022-07-25 14:06:35 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a<=100&&90<=a)
+        printf("A");
+    else if(a<90&&a>=80)
+        printf("B");
+    else if(a<80&&a>=70)
+        printf("C");
+    else if(a<70&&a>=60)
+        printf("D");
+    else
+        printf("F");
+}
