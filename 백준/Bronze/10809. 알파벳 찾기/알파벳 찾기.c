f70d2e24dd8246d6e6c865a18e281e6c0ee28025
chanzerolee <86837078+chanzerolee@users.noreply.github.com>
2022-07-25 14:18:03 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char a[100]={0};
+    scanf("%s",a);
+    for(int i=97;i<=122;i++){
+        int j=0;
+        while(a[j]!=0){
+            if(a[j]==(char)i) break;
+                j++;
+        }
+        if(a[j]==(char)i) printf("%d ",j);
+        else
+            printf("%d ",-1);
+    }
+}
