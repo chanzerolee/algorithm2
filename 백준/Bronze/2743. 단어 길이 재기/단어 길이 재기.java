3255aac15360474bcfbf3c6841fbbe5389e80a83
chanzerolee <86837078+chanzerolee@users.noreply.github.com>
2022-08-24 14:40:00 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 208 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner stdin=new Scanner(System.in);
+	    String s=stdin.next();
+	    System.out.println(s.length());
+	}
+}
